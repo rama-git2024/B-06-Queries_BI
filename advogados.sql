3dbf14ecce1d9f729b5fe76e014456f125b4ee9b
Pedro Vieira Cecere <pedro.cecere@rama.local>
2024-10-23 17:53:52 -0300

    alteração do quadro de advogados

--- advogados.sql
+++ advogados.sql
@@ -10,33 +10,31 @@ SELECT
 	a.F00114 AS data_admissao,
 	a.F00115 AS data_demissao,
 	CASE
-		WHEN a.F00091	IN ('Luize Cezar dos Santos','Jocelaine Rodrigues Machado', 'Luanni dos Santos Jardim') THEN 'Indicações'
-		WHEN a.F00091	IN ('Prescila Koiwaske', 'Danielle Silva de Souza', 'Rafaela Botona Nunes') THEN 'Ajuizamento'
-		WHEN a.F00091	IN ('Manoela Trindade de Pauli', 'Ketlin Caroline Marques Soares') THEN 'Acordos'
-		WHEN a.F00091	IN ('Karla Fernanda Oliveira Paulo', 'Marcos Antonio Rodrigues Junior', 'Jonathan Viegas Avila Cruz') THEN 'Citações'
-		WHEN a.F00091	IN ('Simone da Silva Fagundes', 'Roberto da Silva Lima', 'Bruno Melo Vinhatti', 'Raquel Costa Morussi','Andrea Lessa Gullo', 'Andressa Pereira de Mattos', 'Tarcisio Froes', 'Alexandra Uczak da Gama') THEN 'Penhoras'
-		WHEN a.F00091	IN ('Rosana Teixeira da Silveira', 'Vinicius Cainã Pinheiro', 'Ananda Piegas Piffero') THEN 'Controladoria'
-		WHEN a.F00091	= 'Aline Viganigo de Moraes Alves' THEN 'Recursos'
-		WHEN a.F00091   IN ('Simone da Rosa Godolphim', 'Cariziane de Souza Mauat','Alessandra Borba Longo' ) THEN 'Supervisão'
+		WHEN a.F00091	IN ('Luize Cezar dos Santos','Jocelaine Rodrigues Machado', 'Pedro Henrique da Silva') THEN 'Indicações'
+		WHEN a.F00091	IN ('Prescila Koiwaske', 'Rafaela Botona Nunes', 'Rafaela Roubust dos Santos Garcia') THEN 'Ajuizamento'
+		WHEN a.F00091	IN ('Manoela Trindade de Pauli') THEN 'Acordos'
+		WHEN a.F00091	IN ('Karla Fernanda Oliveira 
+			Paulo', 'Marcos Antonio Rodrigues Junior', 'Vinicius Pereira Souza') THEN 'Citações'
+		WHEN a.F00091	IN ('Simone da Silva Fagundes', 'Roberto da Silva Lima', 'Bruno Melo Vinhatti', 'Raquel Costa Morussi','Andrea Lessa Gullo', 'Andressa Pereira de Mattos', 'Tarcisio Froes', 'Caroline Campos Couto', 'Michelle Gonçalves Pinto', 'Débora Cardoso Motta') THEN 'Penhoras'
+		WHEN a.F00091	IN ('Rosana Teixeira da Silveira', 'Ananda Piegas Piffero', 'Valeska Graboski Moreira', 'Gabriel Galarça Dutra', 'Pedro Henrique Jardim Centeno') THEN 'Controladoria'
+		WHEN a.F00091	IN ('Aline Viganigo de Moraes Alves', 'Andressa Franco Silveira') THEN 'Recursos'
+		WHEN a.F00091   IN ('Simone da Rosa Godolphim', 'Alessandra Borba Longo' ) THEN 'Supervisão'
 		WHEN a.F00091	IN ('Sirlei Maria Rama Vieira Silveira', 'Daniela Peres Marques Silveira') THEN 'Sócia'
 		ELSE NULL
 	END AS nucleo
 FROM [ramaprod].[dbo].T00030 AS a
 LEFT JOIN [ramaprod].[dbo].T00003 AS b ON a.F00091 = b.F00689
 WHERE
-	(CASE
-		WHEN a.F00091	IN ('Luize Cezar dos Santos','Jocelaine Rodrigues Machado', 'Luanni dos Santos Jardim') THEN 'Indicações'
-		WHEN a.F00091	IN ('Prescila Koiwaske', 'Danielle Silva de Souza', 'Rafaela Botona Nunes') THEN 'Ajuizamento'
-		WHEN a.F00091	IN ('Manoela Trindade de Pauli', 'Ketlin Caroline Marques Soares') THEN 'Acordos'
-		WHEN a.F00091	IN ('Karla Fernanda Oliveira Paulo', 'Marcos Antonio Rodrigues Junior', 'Jonathan Viegas Avila Cruz') THEN 'Citações'
-		WHEN a.F00091	IN ('Simone da Silva Fagundes', 'Roberto da Silva Lima', 'Bruno Melo Vinhatti', 'Raquel Costa Morussi','Andrea Lessa Gullo', 'Andressa Pereira de Mattos', 'Tarcisio Froes', 'Alexandra Uczak da Gama') THEN 'Penhoras'
-		WHEN a.F00091	IN ('Rosana Teixeira da Silveira', 'Vinicius Cainã Pinheiro', 'Ananda Piegas Piffero') THEN 'Controladoria'
-		WHEN a.F00091	= 'Aline Viganigo de Moraes Alves' THEN 'Recursos'
-		WHEN a.F00091   IN ('Simone da Rosa Godolphim', 'Cariziane de Souza Mauat','Alessandra Borba Longo' ) THEN 'Supervisão'
+	(CASE 
+		WHEN a.F00091	IN ('Luize Cezar dos Santos','Jocelaine Rodrigues Machado', 'Pedro Henrique da Silva') THEN 'Indicações'
+		WHEN a.F00091	IN ('Prescila Koiwaske', 'Rafaela Botona Nunes', 'Rafaela Roubust dos Santos Garcia') THEN 'Ajuizamento'
+		WHEN a.F00091	IN ('Manoela Trindade de Pauli') THEN 'Acordos'
+		WHEN a.F00091	IN ('Karla Fernanda Oliveira Paulo', 'Marcos Antonio Rodrigues Junior', 'Vinicius Pereira Souza') THEN 'Citações'
+		WHEN a.F00091	IN ('Simone da Silva Fagundes', 'Roberto da Silva Lima', 'Bruno Melo Vinhatti', 'Raquel Costa Morussi','Andrea Lessa Gullo', 'Andressa Pereira de Mattos', 'Tarcisio Froes', 'Caroline Campos Couto', 'Michelle Gonçalves Pinto', 'Débora Cardoso Motta') THEN 'Penhoras'
+		WHEN a.F00091	IN ('Rosana Teixeira da Silveira', 'Ananda Piegas Piffero', 'Valeska Graboski Moreira', 'Gabriel Galarça Dutra', 'Pedro Henrique Jardim Centeno') THEN 'Controladoria'
+		WHEN a.F00091	IN ('Aline Viganigo de Moraes Alves', 'Andressa Franco Silveira') THEN 'Recursos'
+		WHEN a.F00091   IN ('Simone da Rosa Godolphim', 'Alessandra Borba Longo' ) THEN 'Supervisão'
 		WHEN a.F00091	IN ('Sirlei Maria Rama Vieira Silveira', 'Daniela Peres Marques Silveira') THEN 'Sócia'
 		ELSE NULL
-	END) IS NOT NULL;
-
-
-
-
+	END
+	) IS NOT NULL;
